cdbdf4884e04212f076b157d6186d3d127a06c12
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2021-06-10 00:42:30 +0100

    add classes

--- ig_image.py
+++ ig_image.py
@@ -12,34 +12,48 @@ formats = [
     '.bmp'
 ]
 
-def getUrl(link):
-    proxy_ip = input('proxy [None]: ')
-    if proxy_ip != '':
-        http = ProxyManager(proxy_ip, maxsize = 10)
-    else:
-        http = PoolManager()
-    r = http.request('GET', link).data.decode('utf-8')
-    start = r.find('"display_url":"https://scontent') + 15
-    end = r.find('"', start)
-    return r[start:end].replace('\\u0026', '&')
+class IgPost:
 
+    def __init__(self, link):
+        self._link = link
 
-def savePic(pic_url, filename):
-    if filename == '':
-        filename = f'ig_downloaded_pic-{str(datetime.now())}.jpg'
-    elif filename[len(filename) - 4:len(filename)] not in formats and filename[len(filename) - 5:len(filename)] != '.jpeg':
-        filename += '.jpg'
-    urlretrieve(pic_url, filename = filename)
-    return filename
+    def setProxy(self, proxy_ip):
+        if proxy_ip != '':
+            self._http = ProxyManager(proxy_ip, maxsize = 10)
+        else:
+            self._http = PoolManager()
+
+    def getPicUrl(self):
+        r = self._http.request('GET', self._link).data.decode('utf-8')
+        start = r.find('"display_url":"https://scontent') + 15
+        end = r.find('"', start)
+        return r[start:end].replace('\\u0026', '&')
+
+class Pic:
+
+    def __init__(self, pic_url, filename):
+        self._pic_url = pic_url
+        if filename == '':
+            self._filename = f'ig_downloaded_pic-{str(datetime.now())}.jpg'
+        elif filename[len(filename) - 4:len(filename)] not in formats and self._filename[len(filename) - 5:len(filename)] != '.jpeg':
+            self._filename = filename + '.jpg'
+        else:
+            self._filename = filename
+
+    def savePic(self):
+        urlretrieve(self._pic_url, filename = self._filename)
+
+    def openPic(self):
+        if input('open saved image [Y/n]: ') == 'Y':
+            Image.open(self._filename).show()
 
 if __name__ == '__main__':
 
-    link = input('url of the post: ')
-    pic_url = getUrl(link)
-    print(pic_url)
+    ig_post = IgPost(input('url of the post: '))
+    ig_post.setProxy(input('proxy [None]: '))
+    print(ig_post.getPicUrl())
 
-    filename = input(f'file name [ig_downloaded_pic-{str(datetime.now())}.jpg]: ')
-    filename = savePic(pic_url, filename)
-
-    if input('open saved image [Y/n]: ') == 'Y':
-        Image.open(filename).show()
+    pic = Pic(ig_post.getPicUrl(), input(
+        f'file name [ig_downloaded_pic-{str(datetime.now())}.jpg]: '))
+    pic.savePic()
+    pic.openPic()
